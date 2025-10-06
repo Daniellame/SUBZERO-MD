27da1a08c971ad34719043549db681ad3a2647b4
Daniellame <129336307+Daniellame@users.noreply.github.com>
2025-10-06 20:56:11 +0100

    Update settings.js for DANNY-MD

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;2Ik0SBSK#ls152T8Y5M2d1BiK9WU0dDXDGqQLIwT2Ck8bQGtiOe4",
+  OWNER_NUMBER: "×23409015612194", 
+  PREFIX: "?",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
